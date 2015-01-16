b2a19db56f922b4b0b58b154ae8865a75e2afea3
spirosdi <spiros@wantedpixel.com>
2015-01-16 10:33:57 +0200

    removed from filerev the images

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,8 +224,7 @@ module.exports = function (grunt) {
       dist: {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
-          '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= yeoman.dist %>/styles/{,*/}*.css'
         ]
       }
     },
