68db8b35bd183d71ac3386a3165ddf5011917ea4
spiros.dimopulos <spiros.dimopulos@idealo.de>
2017-11-03 11:05:00 +0100

    Added Impressum section; updated bower/npm configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function (grunt) {
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
+  grunt.loadNpmTasks('grunt-contrib-compass');
 
   // Configurable paths for the application
   var appConfig = {
